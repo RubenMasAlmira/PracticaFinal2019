13526a26000932f71363027eb25c1dc948a28946
Miguel-David Orrico Teruel <otm@iesseveroochoa.net>
2019-05-16 16:47:48 +0200

    Añadido control para el id de usuario (solo números)

--- Ruben/src/PanelDeIntroduccionDelUsuario.java
+++ Ruben/src/PanelDeIntroduccionDelUsuario.java
@@ -1,11 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.text.NumberFormat;
 
 public class PanelDeIntroduccionDelUsuario extends JPanel {
-    JTextField usuario=new JTextField(20);
+    JTextField usuario = new JTextField(10);
     PanelDeIntroduccionDelUsuario(){
         add(new JLabel("Por favor,introduzca su número de identificación o pase su tarjeta."));
         add(usuario);
+        usuario.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //super.keyTyped(e);
+                System.out.println();
+                char entrada=e.getKeyChar();
+                if(entrada<'0' || entrada>'9'){
+                    e.consume();
+                }
+            }
+        });
         setLayout(new GridLayout(0,1));
     }
 }
