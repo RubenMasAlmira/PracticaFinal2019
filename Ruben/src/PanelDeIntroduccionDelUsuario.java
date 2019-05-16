1c919f6f8ac0b767b183bc445f388b4b755affde
Rubén Más Almira <rubenmasalmira@gmail.com>
2019-05-16 17:04:10 +0200

    Metodo getEstadioDePoder()

--- Ruben/src/PanelDeIntroduccionDelUsuario.java
+++ Ruben/src/PanelDeIntroduccionDelUsuario.java
@@ -1,22 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.text.NumberFormat;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class PanelDeIntroduccionDelUsuario extends JPanel {
-    JTextField usuario = new JTextField(10);
+    JTextField usuario=new JTextField(20);
     PanelDeIntroduccionDelUsuario(){
         add(new JLabel("Por favor,introduzca su número de identificación o pase su tarjeta."));
         add(usuario);
         usuario.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                //super.keyTyped(e);
-                System.out.println();
                 char entrada=e.getKeyChar();
                 if(entrada<'0' || entrada>'9'){
                     e.consume();
                 }
+                if(usuario.getText().length()==9){
+                    System.out.println("Has llegado a 9 caracteres");
+                    usuario.addKeyListener(new KeyAdapter() {
+                        @Override
+                        public void keyTyped(KeyEvent e) {
+                            e.consume();
+                        }
+                    });
+                }
+
             }
         });
         setLayout(new GridLayout(0,1));
