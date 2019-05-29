f6731c6aaf6da25812213e925c65d661906f0a19
Rubén Más Almira <rubenmasalmira@gmail.com>
2019-05-29 19:01:59 +0200

    Paneles de usuario y Heroes

--- Ruben/src/PanelBotonesDeModificacionYBorrado.java
+++ Ruben/src/PanelBotonesDeModificacionYBorrado.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PanelBotonesDeModificacionYBorrado extends JPanel implements BorraYModifica{
 
     @Override
     public ActionListener borrar() {
-        return null;
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        };
     }
 
     @Override
